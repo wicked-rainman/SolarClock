08fe4ee1973fc599132635cabb8f0cf658e22a07
wicked-rainman <40294416+wicked-rainman@users.noreply.github.com>
2022-06-06 07:14:17 +0100

    Update StepperClient-V01.ino

--- Code/StepperClient-V01.ino
+++ Code/StepperClient-V01.ino
@@ -2,24 +2,19 @@
 #include <WiFi.h>
 #include <WiFiUdp.h>
 #include <Wire.h>
-#include <DFRobot_LSM303.h>
 #include <BH1750.h>
 
 #define DATA_PORT 5003
 #define ROTATE_REQUEST_PORT 5001
 #define ROTATE_REPLY_PORT 5002
-#define CLOCKWISE 1
-#define ANTICLOCKWISE 2
-
-DFRobot_LSM303 compass;
 BH1750 lightMeter;
 IPAddress SendIP(192, 168, 68, 255);
-const char* ssid = "xxxx";
-const char* pass = "xxx";
+const char* ssid = "xxxxx";
+const char* pass = "xxxxx";
 WiFiUDP udpDataOut, udpRotateRequest, udpRotateReply;
-int headingAvg;
 float Lux;
 int LuxInt;
+static int Bearing;
 
 void setup() {
   M5.begin(true, true, true);
@@ -32,90 +27,90 @@ void setup() {
   }
   Wire.begin();
   lightMeter.begin();
-  compass.init();
-  compass.enable();
   udpDataOut.begin(DATA_PORT);
   udpRotateRequest.begin(ROTATE_REQUEST_PORT);
   udpRotateReply.begin(ROTATE_REPLY_PORT);
-  headingAvg = GetHeading();
-  Lux = lightMeter.readLightLevel();
-  LuxInt = round(Lux);
-  //Serial.printf("Light reading = %d\n",LuxInt);
+  Bearing = 0;
   M5.Lcd.fillScreen(BLACK);
-  M5.Lcd.printf("\n%d\n\n%d\n", headingAvg, LuxInt);
+  M5.Lcd.setCursor(1, 0);
+  M5.Lcd.print("Ready");
 }
 
 void loop() {
 
   char payload[40];
   int k;
-  headingAvg = GetHeading();
-
-  while (headingAvg < 210) {
-    if (RequestRotate(CLOCKWISE, 16, 2)) {
-      headingAvg = GetHeading();
+  int brightBearing ;
+  float maxLux;
+  M5.update();
+  if (M5.BtnA.wasReleased()) {
+    brightBearing = 0; maxLux = 0.0;
+    M5.Lcd.fillScreen(RED);
+    RequestRotate(49, 30);
+    M5.Lcd.fillScreen(BLACK);
+    for (Bearing = 50; Bearing < 210; Bearing++) {
+      Bearing = RequestRotate(Bearing, 3);
       Lux = lightMeter.readLightLevel();
+      if (Lux > maxLux) {
+        maxLux = Lux;
+        brightBearing = Bearing;
+      }
       LuxInt = round(Lux);
       M5.Lcd.fillScreen(BLACK);
       M5.Lcd.setCursor(0, 0);
-      M5.Lcd.printf("\n%d\n\n%d\n", headingAvg, LuxInt);
-      Serial.printf("%d,%f\n", headingAvg, Lux);
-      sendData(headingAvg, Lux);
+      M5.Lcd.printf("\n%d\n\n%d\n", Bearing, LuxInt);
+      Serial.printf("%d,%f\n", Bearing, Lux);
+      sendData(Bearing, Lux);
     }
-  }
-  while (headingAvg > 50) {
-    if (RequestRotate(ANTICLOCKWISE, 16, 2)) {
-      headingAvg = GetHeading();
-      Lux = lightMeter.readLightLevel();
-      LuxInt = round(Lux);
       M5.Lcd.fillScreen(BLACK);
       M5.Lcd.setCursor(0, 0);
-      M5.Lcd.printf("\n%d\n\n%d\n", headingAvg, LuxInt);
-      Serial.printf("%d,%f\n", headingAvg, Lux);
-      sendData(headingAvg, Lux);
+      M5.Lcd.print("Done");
+      M5.Lcd.setCursor(0, 30);
+      M5.Lcd.print(brightBearing);
+      Serial.printf("Lightest bearing = %d degrees (Lux value was %f)\n", brightBearing, maxLux);
     }
   }
-}
 
-void sendData(int compassval, float Lux) {
-  char payload[40];
-  memset(payload, 0, 40);
-  snprintf(payload, 40, "%d,%f\n", compassval, Lux);
-  udpDataOut.beginPacket(SendIP, DATA_PORT);
-  udpDataOut.print(payload);
-  udpDataOut.endPacket();
-}
-
-int GetHeading() {
-  float headings;
-  int k;
-  //Take an average of 10 compass readings - it seems to be crap
-  headings = 0.0;
-  for (k = 0; k < 10; k++) {
-    compass.read();
-    headings += compass.getNavigationAngle();
-    delay(25);
+  void sendData(int compassval, float Lux) {
+    char payload[40];
+    memset(payload, 0, 40);
+    snprintf(payload, 40, "%d,%f\n", compassval, Lux);
+    udpDataOut.beginPacket(SendIP, DATA_PORT);
+    udpDataOut.print(payload);
+    udpDataOut.endPacket();
   }
-  return (round(headings / 10.0));
-}
 
-boolean RequestRotate(int Rdirection, int degs, int MaxWaitTime) {
-  char rotateStr[5];
-  char rotateReply[20];
-  int payloadSize = 0, packetSize = 0;
-  int waitTime = 0;
-  snprintf(rotateStr, 5, "%d%d", Rdirection, degs);
-  udpRotateRequest.beginPacket(SendIP, ROTATE_REQUEST_PORT);
-  udpRotateRequest.print(rotateStr);
-  udpRotateRequest.endPacket();
-  //Now wait for a reply to say the servo server has done
-  while (true) {
-    packetSize = udpRotateReply.parsePacket();
-    if (packetSize) {
-      payloadSize = udpRotateReply.read(rotateReply, 20);
-      if ((payloadSize >= 2) && (!strncmp("Ok", rotateReply, 2))) return true;
+
+  int RequestRotate(int bearing, int MaxWaitTime) {
+    char rotateReply[20];
+    int payloadSize = 0, packetSize = 0, reply = 0;;
+    int waitTime = 0;
+
+    sendRequest(bearing);
+    //Now wait for a reply to say the servo server has done the job
+    while (reply != bearing) {
+      packetSize = udpRotateReply.parsePacket();
+      if (packetSize) {
+        memset(rotateReply, 0, 20);
+        payloadSize = udpRotateReply.read(rotateReply, 20);
+        if (payloadSize == 3) reply = atoi(rotateReply);
+        else sendRequest(bearing);
+      }
+      delay(1);
+      if (waitTime++ >= MaxWaitTime * 1000) {
+        Serial.printf("#Repeating request for bearing %d\n", bearing);
+        sendRequest(bearing);
+        waitTime = 0;
+      }
     }
-    delay(1);
-    if (waitTime++ >= MaxWaitTime * 1000) return false;
+    return bearing;
   }
-}
+
+  void sendRequest(int bearing) {
+    char bearingStr[10];
+    memset(bearingStr, 0, 10);
+    snprintf(bearingStr, 5, "%3d", bearing);
+    udpRotateRequest.beginPacket(SendIP, ROTATE_REQUEST_PORT);
+    udpRotateRequest.print(bearingStr);
+    udpRotateRequest.endPacket();
+  }
