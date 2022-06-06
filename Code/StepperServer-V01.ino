3bdbc9c4eecf4befd9e4bc6ed44a996f55d5b094
wicked-rainman <40294416+wicked-rainman@users.noreply.github.com>
2022-06-06 07:16:39 +0100

    Update StepperServer-V01.ino

--- Code/StepperServer-V01.ino
+++ Code/StepperServer-V01.ino
@@ -1,115 +1,144 @@
 #include <M5Atom.h>
 #include <WiFi.h>
 #include <WiFiUdp.h>
-#define SPIN_REQUEST_PORT 5001;
+#define SPIN_REQUEST_PORT 5001
+#define SPIN_REPLY_PORT 5002
 
-#define ROTATION_DELAY 8                //Defines low pulse duration after stepper coil stimulation
-#define HALFSTEP_PULSE_LEN 4            //Defines high pulse duration when stimulating stepper coil - Halfstep
-#define CLOCKWISE true                 //Stepper rotation direction 
+#define ROTATION_DELAY 8                 //Defines low pulse duration after stepper coil stimulation
+#define HALFSTEP_PULSE_LEN 4             //Defines high pulse duration when stimulating stepper coil - Halfstep
+#define CLOCKWISE true                   //Stepper rotation direction 
 #define ANTICLOCKWISE false              //Stepper rotation direction
 
 #define In1 33
 #define In2 23
 #define In3 19
 #define In4 22
+#define Photo 25
 static int StepPosition = 0;
+static int Bearing;
 IPAddress SendIP(192, 168, 68, 255);
-const char* ssid = "xxxx";
-const char* pass = "xxx";
-WiFiUDP udpRX;
+const char* ssid = "xxxxx";
+const char* pass = "xxxxx";
+WiFiUDP udpRX, udpTX;
 
 void setup() {
-  M5.begin(true,true,true);
+  M5.begin(true, true, true);
   pinMode(In1, OUTPUT); digitalWrite(In1, LOW);
   pinMode(In2, OUTPUT); digitalWrite(In2, LOW);
   pinMode(In3, OUTPUT); digitalWrite(In3, LOW);
   pinMode(In4, OUTPUT); digitalWrite(In4, LOW);
+  pinMode(Photo, INPUT);
   Serial.begin(115200);
   WiFi.begin(ssid, pass);
-    while (WiFi.status() != WL_CONNECTED) {
-    M5.dis.drawpix(4,0xff0000);
+  while (WiFi.status() != WL_CONNECTED) {
+    M5.dis.drawpix(4, 0xff0000);
     delay(100);
     M5.dis.clear();
     delay(100);
   }
-  M5.dis.drawpix(4,0x00ff00);
-udpRX.begin(SPIN_REQUEST_PORT);
+  Bearing = 0;
+  StepPosition = 0;
+  M5.dis.drawpix(4, 0x00ff00);
+  udpRX.begin(SPIN_REQUEST_PORT);
+  udpTX.begin(SPIN_REPLY_PORT);
 }
 
 void loop() {
-  int packetSize=0;
-  int payloadSize=0;
-  char packet[100];
+  int packetSize = 0;
+  int payloadSize = 0;
+  char packet[20];
   boolean SpinDirection;
-  char StrStepCount[50];
-  int SpinRequesCount=0;
-  
+  char StrBearing[50];
+  int WantedBearing = 0;
 
-  packetSize=udpRX.parsePacket();
-  if(packetSize) {
-    payloadSize=udpRX.read(packet,50);
-    if((payloadSize > 1) && (payloadSize <50)) {
-      M5.dis.drawpix(9,0xff0000);
-      if(packet[0]== '1') SpinDirection = CLOCKWISE;
-      else SpinDirection=ANTICLOCKWISE;
-      strncpy(StrStepCount,packet+1,(strlen(packet)-1));
-      SpinRequestCount=atoi(StrStepCount);
-      Serial.printf("\"$s\" (%d)\n", StrStepCount, SpinRequestCount);
-      //RotateBase(SpinDirection,SpinRequestCount);
-      M5.dis.drawpix(9,0x000000);
+  packetSize = udpRX.parsePacket();
+  if (packetSize) {
+    memset(packet, 0, 20);
+    payloadSize = udpRX.read(packet, 10);
+    if (payloadSize == 3) {
+      M5.dis.drawpix(9, 0xff0000);
+      WantedBearing = atoi(packet);
+      if (WantedBearing > Bearing) {
+        Serial.printf("Bearing=%d. Wanted=%d. Rotating clockwise\n", Bearing, WantedBearing);
+        while (Bearing != WantedBearing) {
+          RotateBase(CLOCKWISE);
+          Bearing++;
+        }
+      }
+      if (WantedBearing < Bearing) {
+        Serial.printf("Bearing=%d. Wanted=%d. Rotating anti-clockwise\n", Bearing, WantedBearing);
+        while (Bearing != WantedBearing) {
+          RotateBase(ANTICLOCKWISE);
+          Bearing--;
+        }
+      }
+      M5.dis.drawpix(9, 0x000000);
     }
     else {
-      Serial.printf("Invalid packet \"%s\"\n",packet);
+      Serial.printf("Invalid packet (%d) \"%s\"\n", payloadSize, packet);
     }
+    Reply();
   }
 }
 
+void Reply() {
+  char replystr[10];
+  memset(replystr, 0, 10);
+  snprintf(replystr, 5, "%3d", Bearing);
+  udpTX.beginPacket(SendIP, SPIN_REPLY_PORT);
+  udpTX.print(replystr);
+  udpTX.endPacket();
+}
+
 void TogglePixels(int colour, int led1, int led2, int led3, int led4, int led5) {
-  static int islit=1;
+  static int islit = 1;
   switch (islit) {
     case 1 : {
-      M5.dis.drawpix(led5, 0xff000000);
-      M5.dis.drawpix(led1, colour);
-      break;
-    }
+        M5.dis.drawpix(led5, 0xff000000);
+        M5.dis.drawpix(led1, colour);
+        break;
+      }
     case 2 : {
-      M5.dis.drawpix(led1, 0xff000000);
-      M5.dis.drawpix(led2, colour);
-      break; 
-    }
+        M5.dis.drawpix(led1, 0xff000000);
+        M5.dis.drawpix(led2, colour);
+        break;
+      }
     case 3 : {
-      M5.dis.drawpix(led2, 0xff000000);
-      M5.dis.drawpix(led3, colour);
-      break;
-    }
+        M5.dis.drawpix(led2, 0xff000000);
+        M5.dis.drawpix(led3, colour);
+        break;
+      }
     case 4 : {
-      M5.dis.drawpix(led3, 0x000000);
-      M5.dis.drawpix(led4, colour);
-      break;
-    }
+        M5.dis.drawpix(led3, 0x000000);
+        M5.dis.drawpix(led4, colour);
+        break;
+      }
     case 5 : {
-      M5.dis.drawpix(led4, 0x000000);
-      M5.dis.drawpix(led5, colour);
-    }
+        M5.dis.drawpix(led4, 0x000000);
+        M5.dis.drawpix(led5, colour);
+      }
   }
-  if(islit ==5) islit=1;
+  if (islit == 5) islit = 1;
   else islit++;
 }
 
 
-void RotateBase(boolean RotateDirection, int count) {
+void RotateBase(boolean RotateDirection) {
   int k;
-  for (k = 1; k <= count; k++) {
-    if (RotateDirection) {
-      TogglePixels(0x00ff00,0,5,10,15,20);
-      StepPosition++;
-      if (StepPosition > 8) StepPosition = 1;
+  boolean state;
+  state = (boolean) digitalRead(Photo);
+  k = 0;
+  while ((boolean) digitalRead(Photo) == state) {
+    k++;
+    if (RotateDirection == true) {
+      TogglePixels(0x00ff00, 0, 5, 10, 15, 20);
+      StepPosition--;
+      if (StepPosition < 1) StepPosition = 8;
     }
     else {
-      StepPosition--;
-            TogglePixels(0x0000ff,0,5,10,15,20);
-
-      if (StepPosition < 1) StepPosition = 8;
+      StepPosition++;
+      TogglePixels(0x0000ff, 0, 5, 10, 15, 20);
+      if (StepPosition > 8) StepPosition = 1;
     }
     switch (StepPosition) {
       case 1: {
